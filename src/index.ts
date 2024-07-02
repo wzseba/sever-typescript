1e10dd553710c255fd8b7df36b214517ad8360bf
wzseba <wzseba@gmail.com>
2024-07-01 23:22:18 -0300

    refactor: modificacion de import Server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import Server from "./models/server";
+import { Server } from './models'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-
-const server = new Server()
+const server = new Server
 
 server.listen()
