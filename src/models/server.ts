8defc05a2d8cb9765763323477bb640025113127
wzseba <wzseba@gmail.com>
2024-07-07 21:35:18 -0300

    add: configuracion inicial en proceso

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,13 +1,31 @@
 import express, { Application } from 'express'  
+import {productos} from '../routes'
 
 class Server{
     private app: Application
     private port: string
+    private productoPath: string
 
     constructor(){
         this.app = express()
         this.port = process.env.PORT || '8000'
+
+        // Paths
+        this.productoPath = '/api/producto';
+
+        // Conexion a DB
+
+        // middlewares
+
+        // Inicializar rutas
+        this.routes()
+
+        // Inicialiazar conexion db
     }
+
+    routes() {
+        this.app.use(this.productoPath, productos);
+      }
 
     listen(){
         this.app.listen(this.port, ()=>{
