65e3340d018322a74430117eb9e4986ee223edac
wzseba <wzseba@gmail.com>
2024-07-11 23:07:15 -0300

    add: metodo cors

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import { productos } from '../routes/index.js';
 
 export class Server {
@@ -16,11 +17,16 @@ export class Server {
     // Conexion a DB
 
     // middlewares
+    this.middleware();
 
     // Inicializar rutas
     this.routes();
 
     // Inicialiazar conexion db
+  }
+
+  middleware() {
+    this.app.use(cors());
   }
 
   routes() {
